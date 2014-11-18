43817342bdb2265a773f7965f0e528035ea077d2
mtdargen <mtdargen@gmail.com>
2014-11-17 22:35:51 -0800

    Generalized code to let you choose between Seinfeld and CK standup corpora; also, you can now set the size of n-gram the prog uses.  what fun we can have with computers!!!!

--- CERA.py
+++ CERA.py
@@ -3,26 +3,57 @@
 
 import random
 
-print("s3inf3ld\n")
+print("\n\tC.E.R.A.")
+print("COMEDY EXPELLING ROBOT APPARATUS\n")
 
 r = random.Random()
-jerry = open("jerry.txt",'r')
+fnames = ["jerry.txt","louis.txt"]
+n = choice = -1
 
+print("CHOOSE")
+print("1. Jerry Seinfeld")
+print("2. Louis CK")
+choice = int(input(">>> "))
+while choice < 1 or choice > 2:
+    print("\tno")
+    choice = int(input(">>> "))
 
-trigrams = jerry.read().lower().split()
-trigrams = zip(trigrams,trigrams[1:],trigrams[2:])
+print("HOW MANY GRAMS? [2-4]")
+n = int(input("N = "))
+while n < 2 or n > 4:
+    print("\tno")
+    n = int(input("N = "))
+
+f = open(fnames[choice-1],'r')
+
+grams = f.read().lower().split()
+
+if n == 2:
+    grams = zip(grams, grams[1:])
+elif n == 3:
+    grams = zip(grams,grams[1:],grams[2:])
+elif n == 4:
+    grams = zip(grams, grams[1:], grams[2:], grams[3:])
 
 while True:
-    tg = r.choice([x for x in trigrams if x[0] == "<start>"])
-    joke = ""
-    while tg[2] != "<end>":
-            tg = r.choice([x for x in trigrams if (x[0] == tg[1]) and (x[1] == tg[2])])
-            if tg[0] == "<start>":
-                joke += "\n"
-            else:
-                joke += tg[0] + " "
-    joke += tg[1]
-    print(joke)
-    raw_input("\n ... \n")
+        g = r.choice([x for x in grams if x[0] == "<start>"])
+        joke = ""
+        while g[n-1] != "<end>":
 
-jerry.close()
+                if n == 2: g = r.choice([x for x in grams if (x[0] == g[1])])
+                elif n == 3: g = r.choice([x for x in grams if (x[0] == g[1]) and (x[1] == g[2])])
+                elif n == 4: g = r.choice([x for x in grams if (x[0] == g[1]) and (x[1] == g[2]) and (x[2] == g[3])])
+
+                if g[0] == "<start>":
+                    joke += "\n"
+                elif g[0] == "<end>":
+                    break
+                else:
+                    joke += g[0] + " "
+
+        if n == 3: joke += g[1]
+        elif n == 4: joke += g[1] + " " + g[2]
+
+        print(joke)
+        raw_input("\n ... \n")
+f.close()
